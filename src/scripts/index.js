a3308dbdf8a51c392e112485c5ba8218da1b8dba
patrickthumeyer <p.thumeyer@gmail.com>
2020-03-02 13:44:05 +0100

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+// The following line makes sure your styles are included in the project. Don't remove this.
+import "../styles/main.scss";
+import "regenerator-runtime/runtime";
+import "bootstrap";
+import "../../node_modules/bootstrap/dist/css/bootstrap.scss";
+// Import any additional modules you want to include below \/
+
+// \/ All of your javascript should go here \/
